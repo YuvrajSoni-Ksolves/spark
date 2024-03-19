0c1d1cc710d484eefdacac03df9a4bc65cc3baaa
YuvrajSoni-Ksolves <yuvraj.soni@ksolves.com>
2024-03-19 16:37:31 +0530

    rdd to df

---
+++ src/main/scala/Spark_Training/Day2/SparkSQL/RDDtoDF.scala
@@ -0,0 +1,32 @@
+package Spark_Training.Day2.SparkSQL
+
+import Spark_Training.Constants._
+import org.apache.spark.sql._
+import org.apache.spark.sql.types._
+
+object RDDtoDF {
+  def main(args: Array[String]): Unit = {
+    val columns = Seq("language", "users_count")
+    val data = Seq(("Java", 2000), ("Python", 10000), ("Scala", 3000))
+
+    import spark.implicits._
+    val rdd = sc.parallelize(data)
+    //    val dffFromRdd1 = rdd.toDF("langugage", "users_count");
+    //    dffFromRdd1.printSchema()
+    //    dffFromRdd1.show()
+
+    //    val dffFromRdd2 = spark.createDataFrame(rdd).toDF(columns: _*)
+    //    dffFromRdd2.show()
+
+    val schema = StructType(Array(
+      StructField("langugae", StringType, nullable = true),
+      StructField("users", StringType, nullable = true)
+    ))
+
+    //    val rowRdd = rdd.map(attribute => Row(attribute._1, attribute._2))
+    //    val dfFromRDD3 = spark.createDataFrame(rowRdd, schema)
+    //    dfFromRDD3.show()
+
+  }
+
+}
