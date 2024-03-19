316522654e78bab0cedf40b87c0720abbdedc726
YuvrajSoni-Ksolves <yuvraj.soni@ksolves.com>
2024-03-19 13:10:47 +0530

    added spark as a constant

---
+++ src/main/scala/Spark_Training/Constants.scala
@@ -0,0 +1,12 @@
+package Spark_Training
+
+import org.apache.spark.sql._
+
+object Constants {
+
+    val spark = SparkSession.builder()
+      .appName("Spark Training")
+      .master("local[1]")
+      .getOrCreate()
+
+}
